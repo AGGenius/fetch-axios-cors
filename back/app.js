9dc6f8779efad455d11ee7a6cb2c40949a404c71
Adrian <adrianginergimenez@gmail.com>
2024-05-06 10:50:34 +0200

    feat: Complete

---
+++ back/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+
+const port = 3000;
+const route = '/pokemon/:pokemonName'; //Yo inclui un "/" antes de pokemon que en el video no comenta. ¿Sera necesario? Creo que si. TENIA YO RAZON! Je
+
+app.use(cors());
+
+app.get(route, async (req, res) => {
+    const pokeName = req.params['pokemonName']; // Tambien funciona como "req.params.pokemonName"
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`; // Traemos la url y añadimos el pokemon recibido como parametro.
+
+    try {
+        const response = await axios.get(url); // Trabaja como un fetch, realizando los then de forma interna.
+        const {name, sprites: {front_default}, height, weight} = response.data; // Destructuring de datos.
+
+        res.json({name, sprites: {front_default}, height, weight}) // Nos treamos los elementos de tipo "response" como variables en formato JSON, y los convertimos JS.
+    } catch (error) {
+        res.status(404).json({error: `Pokemon no encontrado: ${error}`});
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server working at http://localhost:${port}`);
+})
---
+++ front/js/script.js
@@ -0,0 +1,20 @@
+const infoDOM = document.getElementById('pokemonInfo');
+const pokeInput = document.getElementById('pokemonName');
+
+function getPokemonInfo() {
+    const pokemonName = pokeInput.value.toLocaleLowerCase();
+
+    fetch(`http://localhost:3000/pokemon/${pokemonName}`)
+        .then(response => response.json())
+        .then(data => {
+            const {name, sprites: {front_default}, height, weight} = data; // Destructuring de datos.
+        
+            infoDOM.innerHTML = `
+                <h2>Pokemón: ${name}</h2>
+                <img src='${front_default}' alt='${name} sprite' />
+                <p>Altura: ${height}</p>
+                <p>Peso: ${weight/10} m</p>
+            `;
+        })
+        .catch(error => infoDOM.innerHTML = `<p>Imposible acceder al pokemon: ${error}</p>`);
+}
